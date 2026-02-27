9a3a54f8c5bec1dc7d005f3a352ae7026661d1f0
Harrison Munguambe <harrison.munguambe17@gmail.com>
2026-02-27 10:57:57 +0200

    estrutura do projecto e logo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 ﻿<script setup>
+import { onMounted, onUnmounted, ref } from 'vue'
 import { RouterView } from 'vue-router'
+import SitePreloader from './components/layout/SitePreloader.vue'
 import SiteHeader from './components/layout/SiteHeader.vue'
 import SiteFooter from './components/layout/SiteFooter.vue'
 import ScrollToTop from './components/layout/ScrollToTop.vue'
+
+const showPreloader = ref(true)
+let preloaderTimer = null
+
+onMounted(() => {
+  preloaderTimer = setTimeout(() => {
+    showPreloader.value = false
+  }, 3000)
+})
+
+onUnmounted(() => {
+  if (preloaderTimer) {
+    clearTimeout(preloaderTimer)
+  }
+})
 </script>
 
 <template>
   <div class="page-wrapper">
+    <SitePreloader v-if="showPreloader" />
     <SiteHeader />
     <RouterView />
     <ScrollToTop />
